bbd431d069a4a298565a3487987f944a6e75c25e
nhinq2 <47347404+nhinq2@users.noreply.github.com>
2024-01-12 15:08:26 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/locations/:param1/:param2/:param3",
+        destination: "/locations",
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
